30ba4e7a63d7421e2c17f73fb37c582d60868d27
Mark LeMoine <linclelinkpart5@gmail.com>
2020-10-18 17:23:11 -0700

    First pass at reading config and trying to fetch metadata, not working yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+
+use std::path::PathBuf;
+
+use clap::Clap;
+
+use anagma::config::Config;
+
+#[derive(Clap)]
+pub(crate) struct Opts {
+    pub(crate) item_path: PathBuf,
+    #[clap(long, alias = "config")]
+    pub(crate) config_file: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    let config = opts.config_file.map(|fp| Config::from_file(&fp).unwrap()).unwrap_or_else(Default::default);
+
+    println!("{:?}", config.item_fn);
+    println!("{:?}", config.self_fn);
+    println!("{:?}", config.schema_format);
+
+    let block = anagma::get_with_config(&opts.item_path, &config);
+
+    println!("{:?}", block);
 }
